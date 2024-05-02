b2b830f8d4429edc3b88ae594604bd8517666fde
Johann-S <johann.servoire@gmail.com>
2024-05-02 12:29:55 +0200

    fix(tests): use same tsconfig file

--- tests/client.ts
+++ tests/client.ts
@@ -1,16 +1,17 @@
+/* eslint-disable no-console */
 import parseDuration from 'parse-duration';
 
-import { KuCoinWs } from '../src/index';
-import { delay } from '../src/util';
+import { KuCoinWs } from '../src/index.js';
+import { delay } from '../src/util.js';
 
 const main = async () => {
   const client = new KuCoinWs();
 
-  client.on('error', data => console.error(data));
-  client.on('reconnect', log => console.log(log));
-  client.on('subscriptions', subList => console.log(`subscriptions: ${subList.length}`));
-  const unSubFn = client.on('ticker-BTC/USDT', tickerBtc => console.log(tickerBtc));
-  client.on('candle-BTC/USDT-1m', candleBtc => console.log(candleBtc));
+  client.on('error', (data) => console.error(data));
+  client.on('reconnect', (log) => console.log(log));
+  client.on('subscriptions', (subList) => console.log(`subscriptions: ${subList.length}`));
+  const unSubFn = client.on('ticker-BTC/USDT', (tickerBtc) => console.log(tickerBtc));
+  client.on('candle-BTC/USDT-1m', (candleBtc) => console.log(candleBtc));
 
   await client.connect();
   client.subscribeTickers(['BTC/USDT', 'ETH/USDT']);
